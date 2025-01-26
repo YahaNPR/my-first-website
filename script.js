93a3c461e6bb19f50968489ec0d47f56d36d7085
root <root@v244356.hosted-by-vdsina.com>
2025-01-26 15:22:51 +0300

    Первая версия сайта

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const heading = document.querySelector('h1');
+
+    heading.addEventListener('click', function() {
+        alert('Поебалу по тыкай!!!');
+    });
+});
+
+const button = document.createElement('button');
+button.textContent = 'Показать дату';
+button.className = 'date-botton';
+
+function showDate() {
+    const currentDate = new Date();
+    alert('Сегодня: ' + currentDate.toLocaleDateString());
+}
+
+document.body.appendChild(button);
+
+button.addEventListener('click', showDate);
+
+const form = document.querySelector('.contact-form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Форма отправлена!');
+});
